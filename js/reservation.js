594565db3eab8e887bf9863ac07fbfaccce8223b
Maelle N <maelle.nioche@gmail.com>
2024-09-22 17:33:48 +0200

    Update reservation form styles and add validation message

--- js/reservation.js
+++ js/reservation.js
@@ -4,21 +4,21 @@ import { selectedStation, extractStationName } from './stations.js';
 const modal = document.getElementById('modal');
 const closeModalBtn = document.getElementById('close');
 const openModalBtn = document.getElementById('openModal');
-const canvas = document.getElementById('signature');
 const form = document.getElementById('reservation-form');
 const nameInput = document.getElementById('name');
 const firstnameInput = document.getElementById('firstname');
 
+// Function to hide the modal
 const closeModal = () => {
   modal.style.display = 'none';
 };
 
+// Function to show the modal
 const openModal = () => {
   modal.style.display = 'block';
-  console.log(selectedStation);
 };
 
-// Gestion de l'ouverture de la modale
+// Handle opening the modal when the button is clicked
 openModalBtn.addEventListener('click', () => {
   if (!selectedStation) {
     alert('Veuillez sélectionner une station.');
@@ -28,22 +28,26 @@ openModalBtn.addEventListener('click', () => {
   openModal();
 });
 
-// Gestion de la fermeture de la modale
+// Handle closing the modal when the close button is clicked
 closeModalBtn.addEventListener('click', () => {
   closeModal();
 });
 
-// Fonction pour formater le temps en minutes et secondes
-function formatTime(seconds) {
+// Function to format time in minutes and seconds
+const formatTime = (seconds) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
   return `${String(minutes).padStart(2, '0')}:${String(
     remainingSeconds
-  ).padStart(2, '0')}`;
-}
+  ).padStart(2, '0')}`; // Format as MM:SS
+};
 
-// Fonction pour démarrer le compte à rebours
-function startTimer(duration, display) {
+/**
+ * Function to start the timer for the reservation.
+ * @param {*} duration  The duration of the timer in seconds.
+ * @param {*} display   The element to display the timer.
+ */
+const startTimer = (duration, display) => {
   let timer = duration,
     minutes,
     seconds;
@@ -51,24 +55,25 @@ function startTimer(duration, display) {
     minutes = Math.floor(timer / 60);
     seconds = timer % 60;
     display.textContent = `Temps restant : ${formatTime(timer)}`;
-
+    openModalBtn.disabled = true; // Disable the reservation button while the timer is running
     if (--timer < 0) {
       clearInterval(interval);
-      display.textContent = 'Temps restant : 00:00';
+      display.textContent = 'Temps restant : 00:00'; // Display 00:00 when the timer ends
+      openModalBtn.disabled = false; // Enable the reservation button when the timer ends
     }
-  }, 1000);
-}
+  }, 1000); // Update every second
+};
 
-// Fonction pour valider la réservation
+// Function to handle form submission and reservation
 const handleSubmit = (e) => {
   e.preventDefault();
 
-  // Récupération des champs texte
+  // Retrieve text field values
   const name = nameInput.value.trim();
   const firstname = firstnameInput.value.trim();
 
-  // Vérification des champs
-  const regex = new RegExp('^[a-zA-ZÀ-ÿ-]+$');
+  // Validate the input fields
+  const regex = new RegExp('^[a-zA-ZÀ-ÿ- ]+$');
   if (!regex.test(name) || !regex.test(firstname)) {
     alert('Veuillez saisir un nom et un prénom valides.');
     return;
@@ -79,22 +84,21 @@ const handleSubmit = (e) => {
     return;
   }
 
-  // Vérification de la signature
+  // Check if a signature has been provided
   if (!hasSignature) {
     alert('Veuillez signer dans le canvas.');
     return;
   }
 
-  // Stocker les informations dans une variable
+  // Store reservation information
   const reservationData = {
     name,
     firstname,
-    signature: canvas.toDataURL(),
   };
 
-  // Gestion du timer de réservation
+  // Handle reservation timer display
 
-  // Afficher la balise <p> avec la phrase
+  // Show the reservation confirmation message
   const reservationValidated = document.querySelector('.reservation-validated');
   const reservationValidatedInfo = document.querySelector(
     '.reservation-validated-info'
@@ -102,16 +106,17 @@ const handleSubmit = (e) => {
   reservationValidated.style.display = 'flex';
   const stationName = extractStationName(selectedStation.name);
   reservationValidatedInfo.textContent = `Vélo réservé à la station ${stationName} par ${reservationData.firstname} ${reservationData.name}`;
-  // Obtenir la balise <p> pour afficher la phrase avec le timer
+
+  // Get the element to display the timer message
   const timerMessage = document.getElementById('timer');
 
-  // Initialiser et démarrer le timer (15 minutes)
+  // Initialize and start the timer (15 minutes)
   const fifteenMinutes = 60 * 15;
   startTimer(fifteenMinutes, timerMessage);
 
-  // close the modal
+  // Close the modal
   closeModal();
 };
 
-// Gestion de la soumission du formulaire
+// Handle form submission
 form.addEventListener('submit', handleSubmit);
--- js/signature.js
+++ js/signature.js
@@ -3,10 +3,10 @@ const canvas = document.getElementById('signature');
 // Get the 2D context for drawing
 const ctx = canvas.getContext('2d');
 
-let drawing = false;
-export let hasSignature = false;
+let drawing = false; // Indicates whether the user is currently drawing
+export let hasSignature = false; // Indicates whether a signature has been drawn
 
-// Set the canvas style
+// Set initial drawing styles for the canvas
 ctx.lineWidth = 1;
 ctx.strokeStyle = '#212529';
 
@@ -18,30 +18,31 @@ ctx.strokeStyle = '#212529';
  * @returns {{x: number, y: number}} - An object containing the adjusted X and Y coordinates.
  */
 const getMousePos = (canvas, e) => {
-  const rect = canvas.getBoundingClientRect(); // Get the canvas position relative to the window
-  const scaleX = canvas.width / rect.width;
-  const scaleY = canvas.height / rect.height;
+  const rect = canvas.getBoundingClientRect(); // Get the canvas position relative to the viewport
+  const scaleX = canvas.width / rect.width; // Calculate horizontal scaling factor
+  const scaleY = canvas.height / rect.height; // Calculate vertical scaling factor
 
   return {
-    x: (e.clientX - rect.left) * scaleX, // Adjusted X coordinate with scaling
-    y: (e.clientY - rect.top) * scaleY, // Adjusted Y coordinate with scaling
+    x: (e.clientX - rect.left) * scaleX, // Adjusted X coordinate
+    y: (e.clientY - rect.top) * scaleY, // Adjusted Y coordinate
   };
 };
 
 /**
- * Clear the canvas.
- * This function is called when the "Clear" button is clicked.
+ * Clear the canvas and reset the signature status.
+ * This function is triggered when the "Clear" button is clicked.
  */
 const clearCanvas = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   hasSignature = false;
 };
 
-// Add an event listener to the "Clear" button
+// Add an event listener to the "Clear" button to clear the canvas
 document.getElementById('clearBtn').addEventListener('click', clearCanvas);
 
 canvas.addEventListener('mousedown', (e) => {
   drawing = true;
+  // Get the mouse position on the canvas
   const pos = getMousePos(canvas, e);
   ctx.beginPath(); // Begin a new path
   ctx.moveTo(pos.x, pos.y); // Move the pen to the starting position
@@ -49,13 +50,15 @@ canvas.addEventListener('mousedown', (e) => {
 
 canvas.addEventListener('mousemove', (e) => {
   if (drawing) {
+    // Get the mouse position on the canvas
     const pos = getMousePos(canvas, e);
     ctx.lineTo(pos.x, pos.y); // Draw a line to the new point
     ctx.stroke(); // Stroke the path
   }
 });
 
+// Stop drawing
 canvas.addEventListener('mouseup', () => {
-  drawing = false; // Stop drawing
+  drawing = false;
   hasSignature = true;
 });
